41f3b3b4aa4f11dd938bb19112c802c17739d940
code1218 <skjil1218@gmail.com>
2023-08-21 09:41:26 +0900

    feat: useEffect

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import * as S from "./styles/common";
 import { Route, Routes } from "react-router-dom";
 import Today from "./pages/Today/Today";
 import { useState } from "react";
+import Calendar from "./pages/Calendar/Calendar";
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/today" element={ <Today /> }/>
           <Route path="/upcoming" />
-          <Route path="/calendar" />
+          <Route path="/calendar" element={ <Calendar /> }/>
           <Route path="/stickywall" />
         </Routes>
       </MainLayout>
